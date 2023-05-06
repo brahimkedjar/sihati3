d4f96b24a1e14e6746437964e9bf28ce8cf94000
brahim kedjar <jemskedjar@gmail.com>
2023-05-06 16:54:57 +0200

    add ruby on rails

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
-const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+const { environment } = require('@rails/webpacker');
+
+const xlsx = require('xlsx');
+
+environment.plugins.append('XLSX', new webpack.ProvidePlugin({
+  'XLSX': 'xlsx'
+}));
+
+module.exports = environment;
